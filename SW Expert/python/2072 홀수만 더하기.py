9e45d9875d6637ba6b6acf569e66e3245682e4de
Sungho95 <tjdgh1785@gmail.com>
2022-05-10 23:36:19 +0900

    2022-05-10 update

---
+++ SW Expert/python/2072 홀수만 더하기.py
@@ -0,0 +1,10 @@
+t = int(input())
+for tc in range(1, t+1):
+    arr = list(map(int, input().split()))
+    result = 0
+
+    for i in range(len(arr)):
+        if arr[i] % 2 == 1:
+            result += arr[i]
+
+    print('#%d %d' %(tc, result))
