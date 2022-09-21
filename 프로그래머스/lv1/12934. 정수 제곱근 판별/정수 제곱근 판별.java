5f074e3decd17ceedec418f4fe0e42372a2c8243
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-09-21 23:15:27 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        return sqrt == (long) sqrt ? (long) Math.pow(sqrt + 1, 2) : -1;
+    }
+}
