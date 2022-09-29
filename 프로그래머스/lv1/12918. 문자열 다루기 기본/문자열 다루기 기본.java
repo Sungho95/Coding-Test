4b0c8a4d1a2b43ae98c37f41d800f0ca75f3e67e
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-09-29 21:37:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,14 +1,23 @@
 class Solution {
     public boolean solution(String s) {
-        
         if (s.length() == 4 || s.length() == 6) {
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) < 48 || s.charAt(i) > 57) {
-                    return false;
-                }
+            try {
+                Integer sToInt = Integer.parseInt(s);
+                return true;
+            } catch (Exception e) {
+                return false;
             }
-            return true;
+        }
+        return false;
+        
+//         if (s.length() == 4 || s.length() == 6) {
+//             for (int i = 0; i < s.length(); i++) {
+//                 if (s.charAt(i) < 48 || s.charAt(i) > 57) {
+//                     return false;
+//                 }
+//             }
+//             return true;
             
-        } else return false;
+//         } else return false;
     }
 }
