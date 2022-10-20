bd605187a9c2d9cf605bfd321a1edfb8ced5c554
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-10-21 00:33:38 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -2,10 +2,15 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         
+        // while (n >= a) {
+        //     n -= a;
+        //     answer += b;
+        //     n += b;
+        // }
+        
         while (n >= a) {
-            n -= a;
-            answer += b;
-            n += b;
+            answer += n / a * b;
+            n = n % a + n / a * b;
         }
         
         return answer;
