388a9afa71d52fd48df8b2641754c8581fb43c07
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-11-06 20:51:38 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        
+        Map<String, HashSet<String>> reportedMap = new HashMap<>();
+        Map<String, Integer> answerMap = new HashMap<>();
+        
+        for (int i = 0; i < id_list.length; i++) {
+            HashSet<String> reportSet = new HashSet<>();
+            reportedMap.put(id_list[i], reportSet);
+            answerMap.put(id_list[i], 0);
+        }
+        
+        
+        for (String s : report) {
+            String reporter = s.split(" ")[0];
+            String reported = s.split(" ")[1];
+            reportedMap.get(reported).add(reporter);
+        }
+        
+        for (String reported : reportedMap.keySet()) {
+            HashSet<String> check = reportedMap.get(reported);
+            
+            if (check.size() >= k) {
+                for (String s : check) {
+                    answerMap.put(s, answerMap.get(s) + 1);
+                }
+            }
+        }
+        
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = answerMap.get(id_list[i]);
+        }
+        
+        return answer;
+    }
+}
