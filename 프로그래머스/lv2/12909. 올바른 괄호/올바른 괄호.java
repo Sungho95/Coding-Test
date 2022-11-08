aa8706dc53607f3a0957def0770d766ada6efa36
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-11-08 21:04:54 +0900

    [level 2] Title: 올바른 괄호, Time: 21.50 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && s.charAt(i) == ')') stack.pop();
+            else stack.push(s.charAt(i));
+        }
+        
+        answer = stack.isEmpty() ? true : false;
+        return answer;
+   }
+}
