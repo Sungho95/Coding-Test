33ad1470e73d9e561419df7976023800497fb98f
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-11-18 21:25:54 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.42 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int divisors = 0;
+        
+        for (int i = 1; i <= number; i++) {
+            divisors = divisorsCount(i);
+            if (divisors > limit) {
+                answer += power;
+            } else {
+                answer += divisors;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public static int divisorsCount(int num) {
+        int count = 0;
+        
+        for (int i = 1; i <= (int)Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                count++;
+                if (num / i != i) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
