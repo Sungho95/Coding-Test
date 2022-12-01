ad7754530f079b05b461d68210d96e3b44595239
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-12-02 01:33:42 +0900

    [unrated] Title: 문자열 나누기, Time: 0.51 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,30 +1,6 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        
-        int x = 1;
-        int notX = 0;
-        char ch = s.charAt(0);
-        for (int i = 1; i < s.length() - 1; i++) {
-            
-            if (x == 0) {
-                ch = s.charAt(i);
-                x++;
-                continue;
-            }
-            
-            if (ch == s.charAt(i)) x++;
-            else notX++;
-            
-            if (x == notX) {
-                answer++;
-                x = 0;
-                notX = 0;
-            }
-        }
-        
-        answer++;
-        
         return answer;
     }
 }
