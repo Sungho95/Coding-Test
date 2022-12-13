0f9610771e4d4d1326a1260e1404454c8e575bbb
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2022-12-13 21:50:59 +0900

    [level 2] Title: 소수 찾기, Time: 13.54 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    HashSet<Integer> set = new HashSet<>();
+    public int solution(String numbers) {
+        int answer = 0;
+        recursive("", numbers);
+        
+        Iterator<Integer> it = set.iterator();
+        while (it.hasNext()) {
+            int number = it.next();
+            
+            if (isPrime(number)) answer++;
+        }
+        
+        
+        return answer;
+    }
+    
+    public boolean isPrime(int num) {
+        if (num < 2) return false;
+        
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public void recursive(String comb, String others) {
+        if (!comb.equals(""))
+            set.add(Integer.valueOf(comb));
+
+        for (int i = 0; i < others.length(); i++)
+            recursive(comb + others.charAt(i), others.substring(0, i) + others.substring(i + 1));
+    }
+}
