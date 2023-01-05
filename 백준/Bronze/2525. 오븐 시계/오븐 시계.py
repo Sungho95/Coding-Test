57471b6c05d57f4b49210f23d190da352dc7de73
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-01-05 23:29:18 +0900

    [Bronze III] Title: 오븐 시계, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+c = int(input())
+
+if b + c >= 60:
+    a += (b + c) // 60
+    result = (b + c) % 60
+else:
+    result = b + c
+
+if a >= 24:
+    a -= 24
+
+print(a, result)
