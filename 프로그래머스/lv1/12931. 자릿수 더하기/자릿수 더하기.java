06595d72e7f837f23174fca691bec087c5f42f3b
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-01-14 03:52:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,15 +2,15 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-//         while (n != 0) {
-//             answer += n % 10;
-//             n /= 10;
-//         }
-        String str = String.valueOf(n);
-        
-        for (int i = 0; i < str.length(); i++) {
-            answer += str.charAt(i) - '0';
+        while (n != 0) {
+            answer += n % 10;
+            n /= 10;
         }
+//         String str = String.valueOf(n);
+        
+//         for (int i = 0; i < str.length(); i++) {
+//             answer += str.charAt(i) - '0';
+//         }
 
         return answer;
     }
