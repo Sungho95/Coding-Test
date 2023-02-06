2782557f9975677d018be84d34f848892b8654a8
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-06 22:46:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 208 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.close();
         StringBuilder star = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
