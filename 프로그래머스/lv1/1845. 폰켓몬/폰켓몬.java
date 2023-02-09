50aeb68092c79d2878ee769fbb1e8049bd718af6
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-09 20:39:46 +0900

    [level 1] Title: 폰켓몬, Time: 7.74 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -8,9 +8,7 @@ class Solution {
         // 최대 가져갈 수 있는 포켓몬
         int possible = nums.length / 2;
         // 중복 제거
-        nums = Arrays.stream(nums)
-            .distinct()
-            .toArray();
+        nums = Arrays.stream(nums).distinct().toArray();
         
         // 최대 종류
         int value = nums.length;
