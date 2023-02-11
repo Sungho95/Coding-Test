8910ea9551007c22fabe568af91c16e0da997a54
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-11 14:26:05 +0900

    [level 1] Title: 폰켓몬, Time: 3.74 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,25 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        /*
-        - n / 2 마리를 가져갈 수 있음
-        - 가져갈 수 있는 최대 종류의 수를 구해야 함.
-        - 중복을 제거했을 때,
-            - n / 2 보다 크면 최대 종류는 n / 2
-            - n / 2가 크면 중복 제거한 리스트의 길이가 최대 종류
-        */
+        // hashSet 생성
+        HashSet<Integer> hashSet = new HashSet<>();
         
-        // 최대 가져갈 수 있는 포켓몬
-        int possible = nums.length / 2;
+        // hashSet에 포켓몬 담기(중복 제거됨)
+        for (int i = 0; i < nums.length; i++) {
+            hashSet.add(nums[i]);
+        }
         
-        // 중복 제거
-        nums = Arrays.stream(nums)
-            .distinct()
-            .toArray();
-        
-        // 최대 종류
-        int value = nums.length;
-        
-        return Math.min(possible, value);
+        // 둘 중 작은 것이 최대 종류
+        return Math.min(hashSet.size(), nums.length / 2);
     }
 }
