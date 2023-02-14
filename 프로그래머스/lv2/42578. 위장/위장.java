1efaebb0a743701aed43fe6c8c4e1ff14c2f813b
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-15 00:03:28 +0900

    [level 2] Title: 위장, Time: 0.06 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,20 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        
+        Map<String, Integer> map = new HashMap<>();
+
+        // 의상 종류의 개수 담기
         for (String[] clothe : clothes) {
-            hashMap.put(clothe[1], hashMap.getOrDefault(clothe[1], 0) + 1);
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
         }
-        
-        Iterator<Integer> it = hashMap.values().iterator();
-        
-        while(it.hasNext()) {
-            answer *= it.next().intValue() + 1;
+
+        /**
+         * 종류별 경우의 수
+         * 상의와 하의가 있는 경우
+         * 상의만 입거나 하의만 입을 수도 있음
+         * 최소 1개 이상은 입어야 함 - 아무 것도 입지 않은 경우를 뺴야함
+         * ((상의 + 1) * (하의 + 1)) - 1
+         */
+        Iterator<Integer> it = map.values().iterator();
+
+        while (it.hasNext()) {
+            answer *= it.next() + 1;
         }
-        
-        return answer-1;
+
+        return answer - 1;
     }
 }
