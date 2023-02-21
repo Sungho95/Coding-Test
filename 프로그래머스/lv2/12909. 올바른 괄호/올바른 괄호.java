355b480aeadd348c2c6497b89cd225594d25242d
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-21 23:43:16 +0900

    [level 2] Title: 올바른 괄호, Time: 18.85 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,14 +1,10 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') count++;
-            if (s.charAt(i) == ')') count--;
-            if (count < 0) return false;
-        }
-        
-        return count == 0 ? true : false;
-   }
+        boolean answer = true;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
 }
