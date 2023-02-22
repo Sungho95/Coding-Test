4918259730afa7e69221b8c84a71405ab229f019
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-22 18:49:58 +0900

    [level 2] Title: 주식가격, Time: 37.99 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,30 +1,30 @@
 import java.util.Stack;
 
 class Solution {
-    public int[] solution(int[] prices) {
-        
+        public int[] solution(int[] prices) {
+        // 결과를 담을 배열(prices의 길이)
         int[] answer = new int[prices.length];
+        // 주식 가격의 idx를 담을 스택
         Stack<Integer> stack = new Stack<>();
 
+        // prices 길이만큼 순회
         for (int i = 0; i < prices.length; i++) {
-            
+            // stack이 비어있지 않고, 주식의 가격이 떨어진 경우
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                
-                answer[stack.peek()] = i - stack.peek();
+                answer[stack.peek()] = i - stack.peek(); // 마지막에 담긴 스택에 1
                 stack.pop();
-                
             }
-            
+
+            // 스택에 i 담기
             stack.push(i);
-            
         }
 
+        // 주식 가격이 떨어지지 않은 기간 계산
         while (!stack.isEmpty()) {
-            
             answer[stack.peek()] = prices.length - stack.peek() - 1;
             stack.pop();
-            
         }
+
         return answer;
     }
 }
