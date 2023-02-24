3f12d2c923a475befa1aac019e7997475e43e40e
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-25 00:14:44 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.14 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean solution(String s) {
-//         // 정규 표현식
-//         if (s.length() == 4 || s.length() == 6) {
-//             return s.matches("(^[0-9]*$)");
-//         }
-//         return false;
-        
-        // try catch 사용
+        // 정규 표현식
         if (s.length() == 4 || s.length() == 6) {
-            try {
-                Integer sToInt = Integer.parseInt(s);
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
+            return s.matches("(^[0-9]*$)");
         }
         return false;
+        
+        // // try catch 사용
+        // if (s.length() == 4 || s.length() == 6) {
+        //     try {
+        //         Integer sToInt = Integer.parseInt(s);
+        //         return true;
+        //     } catch (Exception e) {
+        //         return false;
+        //     }
+        // }
+        // return false;
         
 //         // 첫 풀이
 //         if (s.length() == 4 || s.length() == 6) {
