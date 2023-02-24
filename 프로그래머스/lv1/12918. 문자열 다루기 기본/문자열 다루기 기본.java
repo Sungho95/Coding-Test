9a7e2f43242784e291ccdbdec77c3aa3309765dc
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-02-25 00:13:40 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean solution(String s) {
-        // 정규 표현식
-        if (s.length() == 4 || s.length() == 6) {
-            return s.matches("(^[0-9]*$)");
-        }
-        return false;
-        
-        // // try catch 사용
-        // if (s.length() == 4 || s.length() == 6) {
-        //     try {
-        //         Integer sToInt = Integer.parseInt(s);
-        //         return true;
-        //     } catch (Exception e) {
-        //         return false;
-        //     }
-        // }
-        // return false;
-        
-//         // 첫 풀이
+//         // 정규 표현식
 //         if (s.length() == 4 || s.length() == 6) {
-//             for (int i = 0; i < s.length(); i++) {
-//                 if (s.charAt(i) < 48 || s.charAt(i) > 57) {
-//                     return false;
-//                 }
-//             }
-//             return true;
+//             return s.matches("(^[0-9]*$)");
+//         }
+//         return false;
+        
+//         // // try catch 사용
+//         // if (s.length() == 4 || s.length() == 6) {
+//         //     try {
+//         //         Integer sToInt = Integer.parseInt(s);
+//         //         return true;
+//         //     } catch (Exception e) {
+//         //         return false;
+//         //     }
+//         // }
+//         // return false;
+        
+        // 첫 풀이
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) < 48 || s.charAt(i) > 57) {
+                    return false;
+                }
+            }
+            return true;
             
-//         } else return false;
+        } else return false;
     }
 }
