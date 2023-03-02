bc8c46a24acab2b098f5435bf70dd95e49674236
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-02 22:27:49 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,14 +1,21 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        // 결과를 담을 answer 배열
         int[] answer = new int[commands.length];
-        
-        for (int i = 0; i < commands.length; i++) {
-            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+        int idx = 0;
+
+        // i, j, k를 담은 배열 순회
+        for (int[] command : commands) {
+            // i - 1 번째 인덱스부터 j 까지
+            int[] temp = Arrays.copyOfRange(array, command[0] - 1, command[1]);
             Arrays.sort(temp);
-            answer[i] = temp[commands[i][2] - 1];
+
+            // k번째 수
+            answer[idx] = temp[command[2] - 1];
+            idx++;
         }
-        
+
         return answer;
     }
 }
