eaa510a352d2ab640ef93ffd992c55a407da04d6
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-02 22:36:11 +0900

    [level 2] Title: 가장 큰 수, Time: 2.31 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,14 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(int[] numbers) {
+        // numbers의 길이만큼 배열 선언
         String[] answer = new String[numbers.length];
 
+        // numbers 배열의 요소들을 문자열로 변환 후 배열에 담기
         for (int i = 0; i < numbers.length; i++) {
             answer[i] = String.valueOf(numbers[i]);
         }
 
         Arrays.sort(answer, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
+        // 첫 번쨰 요소가 0이면 이후에 모든 요소도 0임
         if (answer[0].equals("0")) {
             return "0";
         }
