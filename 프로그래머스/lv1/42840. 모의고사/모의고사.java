f0b4497ff5a64643e71bdb990d591776c9f2a293
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-09 06:41:57 +0900

    [level 1] Title: 모의고사, Time: 0.56 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,25 +1,33 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public int[] solution(int[] answers) {
+    public List<Integer> solution(int[] answers) {
+        // 1번, 2번, 3번 수포자의 찍는 패턴
         int[] one = {1, 2, 3, 4, 5};
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        // 맞춘 개수를 담을 배열
         int[] score = new int[3];
         
+        // 맞춘 개수 구하기
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == one[i % one.length]) score[0]++;
-            if (answers[i] == two[i % two.length]) score[1]++;
-            if (answers[i] == three[i % three.length]) score[2]++;
+            if (answers[i] == one[i % 5]) score[0]++;
+            if (answers[i] == two[i % 8]) score[1]++;
+            if (answers[i] == three[i % 10]) score[2]++;
         }
         
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        ArrayList<Integer> list = new ArrayList<>();
+        // 가장 많이 맞춘 개수
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
         
+        List<Integer> list = new ArrayList<>();
+        
+        // 가장 많이 맞춘 사람 담기(오름차순)
         for (int i = 0; i < 3; i++) {
-            if (max == score[i]) list.add(i + 1);
+            if (maxScore == score[i]) list.add(i + 1);
         }
         
-        return list.stream().mapToInt(i -> i).toArray();
+        return list;
     }
 }
