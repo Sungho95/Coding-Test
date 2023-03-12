fa54d103eef66bebbdc201736cb8bf53bfe8ea02
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-12 19:35:39 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -2,9 +2,7 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int sum = brown + yellow;
-        
-        // 카펫의 가로와 세로의 길이는 최소 3
-        // 카펫의 가로 길이는 세로 길이와 같거나 세로 길이보다 길어야함.
+
         for (int i = 3; i < sum; i++) {
             int row = i;
             int col = sum / row;
