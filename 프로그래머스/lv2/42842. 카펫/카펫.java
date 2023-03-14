deb2a51f30b4db2cb401763a163628f97a4c8b0b
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-15 00:05:18 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,22 +1,30 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sum = brown + yellow;
-
-        for (int i = 3; i < sum; i++) {
-            int row = i;
-            int col = sum / row;
-            
-            if (row > col) continue;
-            
-            if ((row - 2) * (col - 2) == yellow) {
-                answer[0] = col;
-                answer[1] = row;
-                
-                return answer;
-            } 
+    public int[] solution(int brown, int red) {
+        for(int i=1; i<=red; i++) {
+            if(red%i==0 && (red/i+i)*2+4==brown) {
+                return new int[] {red/i+2, i+2};
+            }
         }
-        
-        return answer;
+        return null;
     }
+//     public int[] solution(int brown, int yellow) {
+//         int[] answer = new int[2];
+//         int sum = brown + yellow;
+
+//         for (int i = 3; i < sum; i++) {
+//             int row = i;
+//             int col = sum / row;
+            
+//             if (row > col) continue;
+            
+//             if ((row - 2) * (col - 2) == yellow) {
+//                 answer[0] = col;
+//                 answer[1] = row;
+                
+//                 return answer;
+//             } 
+//         }
+    
+//         return answer;
+//     }
 }
