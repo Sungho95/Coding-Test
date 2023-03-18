e7129c7ad16a8a0ed5737078bf35a8306e04e877
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-18 17:55:21 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,30 +1,25 @@
 class Solution {
-    public int[] solution(int brown, int red) {
-        for(int i=1; i<=red; i++) {
-            if(red%i==0 && (red/i+i)*2+4==brown) {
-                return new int[] {red/i+2, i+2};
-            }
-        }
-        return null;
-    }
-//     public int[] solution(int brown, int yellow) {
-//         int[] answer = new int[2];
-//         int sum = brown + yellow;
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        // 총 격자 수
+        int sum = brown + yellow;
 
-//         for (int i = 3; i < sum; i++) {
-//             int row = i;
-//             int col = sum / row;
+        // 노란색 격자 수는 1이상, 열은 3부터 시작하게 됨
+        for (int i = 3; i < sum; i++) {
+            int row = i;
+            int col = sum / row;
             
-//             if (row > col) continue;
+            // 가로의 길이가 세로보다 짧으면 안됨
+            if (row > col) continue;
             
-//             if ((row - 2) * (col - 2) == yellow) {
-//                 answer[0] = col;
-//                 answer[1] = row;
-                
-//                 return answer;
-//             } 
-//         }
+            // 가로와 세로의 -2하여 곱한 것이 노란색 격자의 개수
+            if ((row - 2) * (col - 2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+                break;
+            } 
+        }
     
-//         return answer;
-//     }
+        return answer;
+    }
 }
