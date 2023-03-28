43cb90fe90785dab6f5c3557812a0184324809a9
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-03-29 03:03:08 +0900

    [level 1] Title: 체육복, Time: 0.06 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,28 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 3, [1, 2] [2, 3] -> 2
-        HashSet<Integer> lostSet = new HashSet<>();
-        HashSet<Integer> reserveSet = new HashSet<>();
-        
-        for (int i : reserve) reserveSet.add(i);
-        
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
+
+        // 여벌 가져온 사람
+        for (int i : reserve) {
+            reserveSet.add(i);
+        }
+
         for (int i : lost) {
+            // 여벌 가져온 사람이 잃어버린 경우
             if (reserveSet.contains(i)) {
                 reserveSet.remove(i);
             } else {
                 lostSet.add(i);
             }
         }
-        
-        for (int i : reserveSet) {
+
+        // 잃어버린 사람에게 빌려줄 수 있는 경우
+        for (Integer i : reserveSet) {
             if (lostSet.contains(i - 1)) {
                 lostSet.remove(i - 1);
             } else if (lostSet.contains(i + 1)) {
                 lostSet.remove(i + 1);
             }
         }
-        
+
         return n - lostSet.size();
     }
 }
