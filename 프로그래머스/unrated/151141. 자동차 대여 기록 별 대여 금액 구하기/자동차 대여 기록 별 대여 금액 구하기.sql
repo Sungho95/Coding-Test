885943580858cfc988d072cb664472bf2eb86005
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-06-15 00:38:53 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -7,9 +7,9 @@ JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY RH
 LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN DP
     ON CC.CAR_TYPE = DP.CAR_TYPE
     AND DP.DURATION_TYPE = (CASE
-                                WHEN DATEDIFF(END_DATE,START_DATE)+1>='90' THEN '90일 이상'
-                                WHEN DATEDIFF(END_DATE,START_DATE)+1>='30' THEN '30일 이상'
-                                WHEN DATEDIFF(END_DATE,START_DATE)+1>='7' THEN '7일 이상'
+                                WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= '90' THEN '90일 이상'
+                                WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= '30' THEN '30일 이상'
+                                WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= '7' THEN '7일 이상'
                                 ELSE NULL
                             END)
 WHERE CC.CAR_TYPE = '트럭'
