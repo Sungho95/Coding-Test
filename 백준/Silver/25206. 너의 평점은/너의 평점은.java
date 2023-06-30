0ac512884f88d20116ecd3c0fad8c1548f43d4ce
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-07-01 03:14:19 +0900

    [Silver V] Title: 너의 평점은, Time: 236 ms, Memory: 18212 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -24,35 +24,19 @@ public class Main {
 
         System.out.printf("%f", result);
     }
-    
+
     private static double gradeCalculator(double score, String grade) {
         double sum = 0;
-        
+
         switch (grade) {
-            case "A+":
-                sum += score * 4.5;
-                break;
-            case "A0":
-                sum += score * 4.0;
-                break;
-            case "B+":
-                sum += score * 3.5;
-                break;
-            case "B0":
-                sum += score * 3.0;
-                break;
-            case "C+":
-                sum += score * 2.5;
-                break;
-            case "C0":
-                sum += score * 2.0;
-                break;
-            case "D+":
-                sum += score * 1.5;
-                break;
-            case "D0":
-                sum += score * 1.0;
-                break;
+            case "A+" -> sum += score * 4.5;
+            case "A0" -> sum += score * 4.0;
+            case "B+" -> sum += score * 3.5;
+            case "B0" -> sum += score * 3.0;
+            case "C+" -> sum += score * 2.5;
+            case "C0" -> sum += score * 2.0;
+            case "D+" -> sum += score * 1.5;
+            case "D0" -> sum += score;
         }
         return sum;
     }
