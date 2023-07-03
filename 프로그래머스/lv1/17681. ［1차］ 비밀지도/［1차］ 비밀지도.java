1e6d5003eae256b5dc4a02fee2cdf0b009472a21
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-07-04 00:05:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.85 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,52 +1,31 @@
-// class Solution {
-//     public String[] solution(int n, int[] arr1, int[] arr2) {
-//         String[] answer = new String[n];
-        
-//         for (int i = 0; i < n; i++) {
-//             String str1 = "";
-//             String str2 = "";
-//             while(arr1[i] != 0) {
-//                 str1 = arr1[i] % 2 + str1;
-//                 arr1[i] /= 2;
-//             }
-//             while(arr2[i] != 0) {
-//                 str2 = arr2[i] % 2 + str2;
-//                 arr2[i] /= 2;
-//             }
-            
-//             while (str1.length() != n) str1 = "0" + str1;
-//             while (str2.length() != n) str2 = "0" + str2;
-            
-//             String resultStr = "";
-//             for (int j = 0; j < n; j++) {
-//                 if (str1.charAt(j) == '0' && str2.charAt(j) == '0') {
-//                     resultStr += " ";
-//                 } else {
-//                     resultStr += "#";
-//                 }
-//             }
-            
-//             answer[i] = resultStr;
-//         }
-//         return answer;
-//     }
-// }
-import java.util.Arrays;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] result = new String[n];
-
+        String[] answer = new String[n];
+        
         for (int i = 0; i < n; i++) {
-            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            String str1 = intToBinaryString(arr1[i], n);
+            String str2 = intToBinaryString(arr2[i], n);
+            
+            String resultStr = "";
+            for (int j = 0; j < n; j++) {
+                if (str1.charAt(j) == '0' && str2.charAt(j) == '0') {
+                    resultStr += " ";
+                } else {
+                    resultStr += "#";
+                }
+            }
+            answer[i] = resultStr;
         }
-
-        for (int i = 0; i < n; i++) {
-            result[i] = String.format("%" + n + "s", result[i]);
-            result[i] = result[i].replaceAll("1", "#");
-            result[i] = result[i].replaceAll("0", " ");
+        return answer;
+    }
+    
+    public String intToBinaryString(int target, int n) {
+        String str = Integer.toBinaryString(target);
+        
+        while (str.length() < n) {
+            str = "0" + str;
         }
-
-        return result;
+        
+        return str;
     }
 }
