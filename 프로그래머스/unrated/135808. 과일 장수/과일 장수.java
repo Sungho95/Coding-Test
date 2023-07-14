c7d3efcfe2622fc307a9ef603641267068e55deb
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-07-14 23:47:53 +0900

    [unrated] Title: 과일 장수, Time: 2.74 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,18 +1,10 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        // 한 상자에 m개
-        // k = 4, m = 3
-        // [4 1 2 2 4 4 4 4 1 2 4 2]
-        // 4 4 4  12
-        // 4 4 4  12
-        // 2 2 2  6
-        // 2 1 1  3
-        // 12 + 12 + 6 + 3 = 33
-        // k = 3 m = 4
-        // 1231231
+
         Integer[] scoreDesc = Arrays.stream(score).boxed().toArray(Integer[]::new);
         Arrays.sort(scoreDesc, Collections.reverseOrder());
         
