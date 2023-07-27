7cceabe0ddb2d4e4b929416444697ac660f1dd64
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-07-27 20:35:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.04 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,26 @@
 import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
 
         // 같은 이름의 완주자가 있을 수 있기 때문에 HashMap 사용
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
 
         for (String s : participant) {
             // key가 없는 경우 1, 있을 경우 +1
-            hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
         for (String s : completion) {
             // 완주한 사람의 1명씩 제거
-            hashMap.put(s, hashMap.get(s) - 1);
+            map.put(s, map.get(s) - 1);
         }
 
-        for (String s : hashMap.keySet()) {
+        for (String s : map.keySet()) {
             // 완주하지 못한 사람
-            if (hashMap.get(s) > 0) {
+            if (map.get(s) > 0) {
                 answer = s;
                 break;
             }
