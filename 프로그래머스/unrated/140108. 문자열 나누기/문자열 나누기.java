8f4b135b393b78ccda00f039bbcaa1a84d485033
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-07-29 01:16:24 +0900

    [unrated] Title: 문자열 나누기, Time: 0.82 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -2,22 +2,32 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        int x = 1;
-        char ch = s.charAt(0);
+        int count = 1; // 현재 문자의 개수를 기록하는 변수
+        char prevChar = s.charAt(0); // 이전 문자를 기록하는 변수
+        
         for (int i = 1; i < s.length(); i++) {
-            if (x == 0) {
-                ch = s.charAt(i);
-                x++;
+            char currentChar = s.charAt(i); // 현재 문자를 가져옴
+            
+            if (count == 0) {
+                prevChar = currentChar;
+                count++;
                 continue;
             }
             
-            if (ch == s.charAt(i)) x++;
-            else x--;
+            if (prevChar == currentChar) {
+                count++; // 이전 문자와 현재 문자가 같으면 count 증가
+            } else {
+                count--; // 이전 문자와 현재 문자가 다르면 count 감소
+            }
             
-            if (x == 0) answer++;
+            if (count == 0) {
+                answer++; // count가 0이 되면 하나의 덩어리가 완성되었으므로 answer 증가
+            }
         }
         
-        if (x != 0) answer++;
+        if (count != 0) {
+            answer++; // 남아있는 문자가 하나의 덩어리를 이루므로 answer 증가
+        }
                 
         return answer;
     }
