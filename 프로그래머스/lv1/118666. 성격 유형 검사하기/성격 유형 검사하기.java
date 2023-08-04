4dbb1bf02c8361dee3c4c9f38c894454ceaf89dd
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-08-04 20:37:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.70 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         char[] type = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
 
         Map<Character, Integer> map = new HashMap<>();
@@ -11,31 +11,31 @@ class Solution {
         for (int i = 0; i < type.length; i++) {
             map.put(type[i], 0);
         }
-        
+
         for (int i = 0; i < survey.length; i++) {
             char left = survey[i].charAt(0);
             char right = survey[i].charAt(1);
+
             if (choices[i] == 4) {
                 continue;
             } else if (choices[i] < 4) {
-                map.replace(left, map.get(left) + (4 - choices[i]));
+                map.put(left, map.get(left) + (4 - choices[i]));
             } else {
-                map.replace(right, map.get(right) + (choices[i] - 4));
+                map.put(right, map.get(right) + (choices[i] - 4));
             }
         }
 
         for (int i = 0; i < type.length; i += 2) {
             char left = type[i];
-            char right = type[i+1];
-            if (map.get(left) > map.get(right)) {
-                answer += left;
-            } else if (map.get(left) == map.get(right)) {
-                answer += left;
+            char right = type[i + 1];
+
+            if (map.get(left) >= map.get(right)) {
+                answer.append(left);
             } else {
-                answer += right;
+                answer.append(right);
             }
         }
 
-        return answer;
+        return answer.toString();
     }
 }
