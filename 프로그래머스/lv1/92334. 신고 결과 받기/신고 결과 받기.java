f6dd29c3375fb053f5b7f73764f4dca720340b61
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-08-08 00:15:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
