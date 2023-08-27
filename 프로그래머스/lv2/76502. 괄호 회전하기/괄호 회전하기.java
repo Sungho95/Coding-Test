002e905a9ed56a8e3b060ac7e0ddcc2c032ddecc
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-08-27 20:32:02 +0900

    [level 2] Title: 괄호 회전하기, Time: 10.15 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,28 +1,30 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        
+
         for (int i = 0; i < s.length(); i++) {
             if (verification(s)) {
                 answer++;
             }
             s = shift(s);
         }
-        
+
         return answer;
     }
-    
+
     public boolean verification(String s) {
         Stack<Character> stack = new Stack<>();
-        
+
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stack.push(s.charAt(i));
             } else {
-                if (stack.size() == 0) return false;
-                
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
                 if (stack.peek() == '(' && s.charAt(i) == ')')  {
                     stack.pop();
                 } else if (stack.peek() == '{' && s.charAt(i) == '}') {
@@ -32,11 +34,11 @@ class Solution {
                 } else return false;
             }
         }
-        
-        return stack.size() == 0 ? true : false;
+
+        return stack.isEmpty();
     }
-    
+
     public String shift(String s) {
-        return s.substring(1, s.length()) + s.charAt(0);
+        return s.substring(1) + s.charAt(0);
     }
 }
