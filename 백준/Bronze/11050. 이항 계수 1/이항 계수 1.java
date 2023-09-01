86effcbd97a7984a638e1e22749fc143113d6c8b
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-02 01:57:29 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -10,15 +10,20 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        System.out.println(recursion(n, k));
-
+        System.out.println(dynamic(n, k));
     }
 
-    private static int recursion(int n, int k) {
+    private static int dynamic(int n, int k) {
+        int[][] dp = new int[n + 1][k + 1];
+        
+        if (dp[n][k] > 0) {
+            return dp[n][k];
+        }
+        
         if (n == k || k == 0) {
-            return 1;
+            return dp[n][k] = 1;
         }
 
-        return recursion(n - 1, k - 1) + recursion(n - 1, k);
+        return dp[n][k] = dynamic(n - 1, k - 1) + dynamic(n - 1, k);
     }
 }
