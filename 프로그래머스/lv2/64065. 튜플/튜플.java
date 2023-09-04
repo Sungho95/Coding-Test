7a25a3e5864b1d7a1c331d47c16662b0bb2ef6bc
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-04 16:26:58 +0900

    [level 2] Title: 튜플, Time: 2.27 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,24 +1,27 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 class Solution {
-    public ArrayList<Integer> solution(String s) {
+    public List<Integer> solution(String s) {
         s = s.substring(2, s.length()- 2).replace("},{", "-");
         String[] sArr = s.split("-");
-        
-        Arrays.sort(sArr, (a, b) -> Integer.compare(a.length(), b.length()));
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        
+
+        Arrays.sort(sArr, Comparator.comparingInt(String::length));
+
+        List<Integer> list = new ArrayList<>();
+
         for (String el : sArr) {
             String[] check = el.split(",");
-            
+
             for (int i = 0; i < check.length; i++) {
                 int num = Integer.parseInt(check[i]);
-                
+
                 if (!list.contains(num)) list.add(num);
             }
         }
-        
+
         return list;
     }
 }
