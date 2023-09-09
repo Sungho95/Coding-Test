9eec206a826a92799c913c4c4effc2ab2a93c5ce
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-09 21:19:12 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.39 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,44 +1,41 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
         List<String> list1 = stringSplit(str1);
         List<String> list2 = stringSplit(str2);
 
-        if (list1.size() == 0 && list2.size() == 0) return 65536;
-        
+        if (list1.isEmpty() && list2.isEmpty()) {
+            return 65536;
+        }
+
         List<String> union = new ArrayList<>();
         List<String> inter = new ArrayList<>();
-        
+
         for (String s : list1) {
             if (list2.remove(s)) {
                 inter.add(s);
             }
             union.add(s);
         }
+
+        union.addAll(list2);
+        double jacquard = (double) inter.size() / (double) union.size();
         
-        for (String s : list2) {
-            union.add(s);
-        }
-        
-        double jakard = (double) inter.size() / (double) union.size();
-        return (int) (jakard * 65536);
-        
+        return (int) (jacquard * 65536);
     }
-    
+
     public List<String> stringSplit(String str) {
         List<String> list = new ArrayList<>();
         str = str.toLowerCase();
-        
+
         for (int i = 0; i < str.length() - 1; i++) {
             String s = str.substring(i, i + 2).replaceAll("[^a-z]", "");
             if (s.length() < 2) continue;
             list.add(s);
         }
-        
+
         return list;
     }
-    
 }
